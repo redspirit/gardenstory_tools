54f7503336f8b9b295ba3ec7ef02a0494173847b
Alexey <redspirit@live.ru>
2021-10-08 20:52:38 +0300

    update parser

--- parser/PEParser.js
+++ parser/PEParser.js
@@ -20,23 +20,11 @@ class PEParser {
                 if(err) return reject(err);
                 this.buffer = buf;
 
-                console.log(buf);
+                this.headers = this.getHeaders();
+                this.sections = this.getSections();
+                this.strings = this.extractStrings();
 
-                let groups = [];
-                let string = '';
-                let addr = 0;
-
-                for (const char of buf) {
-                    if(char >= 32 && char <= 126) {
-                        string += String.fromCharCode(char)
-                    } else if(string.length > 0) {
-                        if(string.length > 2) groups.push([(addr - string.length).toString(16), string]);
-                        string = '';
-                    }
-                    addr++;
-                }
-
-                this.strings = groups;
+                // console.log(this.strings);
 
                 resolve(buf);
             });
@@ -44,6 +32,7 @@ class PEParser {
         })
     }
 
+    // взять строку из буфера
     getString(address, length, subBuffer = null) {
         let sbuf = subBuffer
             ? subBuffer.slice(address, address + length)
@@ -55,13 +44,59 @@ class PEParser {
         return str;
     }
 
+    // отфильтровать все найденные строки среди уже найденных при загрузке файла
     findStrings(name) {
 
         return this.strings.filter(item => {
-            return new RegExp(name, "i").test(item[1]);
+            return new RegExp(name, "i").test(item.data);
         }).map(item => {
-            return {address: item[0], data: item[1]}
+            item.section = this.getSectionByOffset(item.offset);
+            item.address = this.convertOffsetToVirtual(item.offset);
+            item.addressHex = this.intToHex(item.address);
+            return item;
         });
+
+    }
+
+    getSectionByOffset(offset) {
+
+        return this.sections.filter(section => {
+            let start = section.pointerToRawData;
+            let end = start + section.sizeOfRawData;
+            return offset >= start && offset < end;
+        })[0];
+
+    }
+
+    convertOffsetToVirtual (offset) {
+        let section = this.getSectionByOffset(offset);
+        if(!section) return null;
+        return offset - section.pointerToRawData + section.virtualAddress + this.headers.imageBase;
+    }
+
+    // извлечь все строки из исполняемом файле (перед этим все секции должны быть найдены)
+    extractStrings () {
+
+        let startOfSections = this.sections[0].pointerToRawData;
+        let buf = this.buffer.slice(startOfSections);
+        let groups = [];
+        let string = '';
+        let addr = startOfSections;
+
+        for (const char of buf) {
+            if(char >= 32 && char <= 126) {
+                string += String.fromCharCode(char)
+            } else if(string.length > 0) {
+                if(string.length > 2) groups.push({
+                    offset: addr - string.length,
+                    data: string,
+                });
+                string = '';
+            }
+            addr++;
+        }
+
+        return groups;
 
     }
 
@@ -81,7 +116,7 @@ class PEParser {
             characteristics: this.intToHex(buf.readUInt16LE(peHeaderAddress + 22)),
             magic: this.intToHex(buf.readUInt16LE(peHeaderAddress + 24)),
             addressOfEntryPoint: this.intToHex(buf.readUInt32LE(peHeaderAddress + 40)),
-            imageBase: this.intToHex(buf.readUInt32LE(peHeaderAddress + 52)),
+            imageBase: buf.readUInt32LE(peHeaderAddress + 52),
             // imageBase: this.intToHex(buf.readBigUInt64LE(peHeaderAddress + 48)), // for 64
             sectionAlignment: this.intToHex(buf.readUInt32LE(peHeaderAddress + 56)),
             fileAlignment: this.intToHex(buf.readUInt32LE(peHeaderAddress + 60)),
@@ -98,28 +133,20 @@ class PEParser {
         let sectionsCount = this.getHeaders()['numberOfSections'];
         let headersSize = this.getHeaders()['sizeOfOptionalHeader'];
         let startAddress = this.buffer.readUInt32LE(60) + headersSize + 24;
-
         let sections = [];
-
         for(let i = 0; i < sectionsCount; i++) {
             let addr = startAddress + i * 40;
             let sect = this.buffer.slice(addr, addr + 40);
-
             sections.push({
                 name: this.getString(0, 8, sect),
-                virtualSize: this.intToHex(sect.readUInt32LE(8)),
-                virtualAddress: this.intToHex(sect.readUInt32LE(12)),
-                sizeOfRawData: this.intToHex(sect.readUInt32LE(16)),
-                pointerToRawData: this.intToHex(sect.readUInt32LE(20)),
+                virtualSize: sect.readUInt32LE(8),
+                virtualAddress: sect.readUInt32LE(12),
+                sizeOfRawData: sect.readUInt32LE(16),
+                pointerToRawData: sect.readUInt32LE(20),
                 characteristics: sect.readUInt32LE(36),
             });
-
-            // console.log(sect);
         }
-
-        // console.log('sections', sections);
         return sections;
-
     }
 
 }
--- parser/app.js
+++ parser/app.js
@@ -16,11 +16,7 @@ const start = async () => {
     let result = parser.findStrings('a game');
     console.log(result);
 
-    console.log(parser.getHeaders());
-    console.log(parser.getSections());
-
     // todo
-    // 1. поиск строки и его виртуального адреса. Formula: offset - pointerToRawData + virtualAddress + imageBase
     // 2. поиск мест, где этот адрес используется
     // 3. добавление новой секции .strings со своими данными
 
